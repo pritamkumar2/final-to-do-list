dbe996b893eadf64564ae1e8c3cf520bcf22b5ab
pritam <pritamram0@gmail.com>
2023-09-29 16:28:17 +0530

    discribe how website backend work

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// import neccessary npm module and run express
 import express from "express";
 import ejs from "ejs";
 import mongoose from "mongoose";
@@ -6,60 +7,77 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// ///////////////////////////
+
+// conection for mongodb and create an db
 const uri = "mongodb://127.0.0.1/toList";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+/////////////////////////
 
+// creating a schema to create format of collection
 const itemList = new mongoose.Schema({
   name: String,
 });
 
+/////create a collection
 const product = new mongoose.model("Item", itemList);
 
-app.get("/", async (req, res) => {
-  const weeks = [
-    "sunday",
-    "monday",
-    "tuesday",
-    "wednesday",
-    "thursday",
-    "friday",
-    "saturday",
-  ];
-  const yearMonths = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let days = new Date();
-  const year = days.getFullYear();
-  const month = yearMonths[days.getMonth()];
-  const day = days.getDay();
-  const today = weeks[day];
+// /////////
 
+// array to find week month day etc
+const weeks = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+const yearMonths = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let days = new Date();
+const year = days.getFullYear();
+const month = yearMonths[days.getMonth()];
+const day = days.getDay();
+const today = weeks[day];
+
+/////////////////////////////////////
+// create an route and endpoint
+
+app.get("/", async (req, res) => {
   try {
     // Fetch the list data from the database
     const data = await product.find();
-
+    // send file to index.ejs
     res.render("../view/index.ejs", {
       day: today,
       month: month,
       year: year,
       newLiItems: data,
     });
+    /////////////
   } catch (err) {
     console.error(err);
     res.redirect("/");
   }
 });
+
+////////////////////////
+
+///////getting data form the form
 
 app.post("/", async (req, res) => {
   let newTask = req.body.newItem;
@@ -78,36 +96,7 @@ app.post("/", async (req, res) => {
 
       // Fetch the updated list data from the database
       const updatedData = await product.find();
-
-      // Render the EJS template with the updated data
-      const weeks = [
-        "sunday",
-        "monday",
-        "tuesday",
-        "wednesday",
-        "thursday",
-        "friday",
-        "saturday",
-      ];
-      const yearMonths = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-      ];
-      let days = new Date();
-      const year = days.getFullYear();
-      const month = yearMonths[days.getMonth()];
-      const day = days.getDay();
-      const today = weeks[day];
+      console.log(updatedData);
 
       res.render("../view/index.ejs", {
         day: today,
@@ -121,10 +110,9 @@ app.post("/", async (req, res) => {
     }
   }
 });
-
+// delete the list items
 app.post("/delete", async (req, res) => {
   const check = req.body.checker;
-  console.log(check , "->the data from form<-");
 
   try {
     // Use findOneAndDelete to find and delete the object by name
@@ -146,6 +134,7 @@ app.post("/delete", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
+// running the server on port 3000
 app.listen(port, (req, res) => {
   console.log(`listening on port ${port}`);
 });
